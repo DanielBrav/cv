e1057a19b2d0dfc07e1889e24bb4b4125c3302ee
Daniel Braverman <bravdani@post.bgu.ac.il>
2019-11-18 10:45:38 +0200

    Updates

---
+++ precache-manifest.784e4b3cf20c61d982b7c0456eb924e8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e0ed8da1b3fc7d633628760f2b01ff",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "42727dc8511150bee974",
+    "url": "/cv/static/css/main.9d170db1.chunk.css"
+  },
+  {
+    "revision": "2c69eb8c6514c6f3b844",
+    "url": "/cv/static/js/2.64351244.chunk.js"
+  },
+  {
+    "revision": "42727dc8511150bee974",
+    "url": "/cv/static/js/main.c2fb6072.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  },
+  {
+    "revision": "78aa8deedd2dbf43561a517e7880b2e2",
+    "url": "/cv/static/media/bg.78aa8dee.jpg"
+  },
+  {
+    "revision": "668bdcdb0ce666df2cdf60e312f929d1",
+    "url": "/cv/static/media/bgulogo.668bdcdb.png"
+  },
+  {
+    "revision": "8023d067807a1842372297b5a448cb2d",
+    "url": "/cv/static/media/code.8023d067.jpeg"
+  },
+  {
+    "revision": "e085d64d4a89325ccde8b99409319be1",
+    "url": "/cv/static/media/experience.e085d64d.jpeg"
+  },
+  {
+    "revision": "b6fcbe4cd5a7ea9db9fd2ec8ba5cac90",
+    "url": "/cv/static/media/haderalogo.b6fcbe4c.jpg"
+  },
+  {
+    "revision": "263093930dee9d813622a92b1158e619",
+    "url": "/cv/static/media/logo.26309393.jpg"
+  },
+  {
+    "revision": "a5b7d642fdf6326ab5b3ea69ea93fc35",
+    "url": "/cv/static/media/study.a5b7d642.jpeg"
+  }
+]);
